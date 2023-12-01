db33dab7225d9a020c8f6f7b64fcf0d1292ab4fc
Serhii Shmyhelskyi <siro181a@gmai.com>
2023-12-01 15:48:57 +0200

    fix problem in footer and contact

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      © 2023 Shm Inc. Designed by Serhii Shmyhelskyi
+      © 2023 Shm Inc. Designed by <br /> Serhii Shmyhelskyi
     </footer>
   );
 };

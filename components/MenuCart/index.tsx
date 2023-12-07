33320be097c9a2296955af4cbcc91bcea6df62ca
Serhii Shmyhelskyi <siro181a@gmai.com>
2023-12-07 19:37:46 +0200

    fiz animation in menuCart

--- components/MenuCart/index.tsx
+++ components/MenuCart/index.tsx
@@ -1,9 +1,8 @@
 "use content";
 import React, { FC, useState } from "react";
 
-import { CSSTransition } from "react-transition-group";
-
 import styles from "./menuCart.module.scss";
+import classnames from "classnames";
 
 type CategoriesProps = {
   href: string;
@@ -20,29 +19,20 @@ const MenuCart: FC<CategoriesProps> = ({ href, text, price, about }) => {
         <h4 className={styles.menu__boxTitleContent}>{text}</h4>
         <img src={href} alt="menu_img" />
         <h4>{price}</h4>
-        <p className={styles.menu__boxText}>
-          {about.substring(0, 40)} {!isFavorite && "..."}
-        </p>
-
-        <CSSTransition
-          in={isFavorite}
-          timeout={300}
-          classNames="my-node"
-          unmountOnExit>
-          <p className={styles.menu__boxText}>
-            {about.substring(40, about.length)}
-          </p>
-        </CSSTransition>
-
-        {/* {isFavorite ? (
-          <p className={styles.menu__boxText}>
-            {about.substring(0, about.length)}
-          </p>
-        ) : (
-          <p className={styles.menu__boxText}>
+        {!isFavorite && (
+          <p className={styles.menu__preBoxText}>
             {about.substring(0, 40) + "..."}
           </p>
-        )} */}
+        )}
+
+        <p
+          className={classnames({
+            [styles.menu__boxText]: !isFavorite,
+            [styles.menu__boxTextOpen]: isFavorite,
+          })}>
+          {about.substring(0, about.length)}
+        </p>
+
         <p className={styles.pMax} onClick={() => setIsFavorite(!isFavorite)}>
           {isFavorite ? "Load min" : "Load more"}
         </p>

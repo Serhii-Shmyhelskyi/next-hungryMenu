d717674715b46f3f9c0b9fe4f8812c16f151b24c
Serhii Shmyhelskyi <siro181a@gmai.com>
2023-12-08 13:33:31 +0200

    fix min-height in menu

--- components/MenuCart/index.tsx
+++ components/MenuCart/index.tsx
@@ -28,7 +28,7 @@ const MenuCart: FC<CategoriesProps> = ({ href, text, price, about }) => {
           })}>
           {isFavorite
             ? about.substring(0, about.length)
-            : about.substring(0, 60)}
+            : about.substring(0, 55)}
           <button
             className={styles.menu__boxTextButton}
             onClick={() => setIsFavorite(!isFavorite)}>

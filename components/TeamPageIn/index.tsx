5e033b9b0d0bc9e067b48ad8fe9f592d6ef01f0d
Serhii Shmyhelskyi <siro181a@gmai.com>
2023-12-13 14:07:35 +0200

    added teamPageIn

---
+++ components/TeamPageIn/index.tsx
@@ -0,0 +1,57 @@
+import { Metadata } from "next";
+import Link from "next/link";
+import styles from "./teamPageIn.module.scss";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+type postType = {
+  id: number;
+  name: string;
+  text: string;
+  imageUrl: string;
+};
+
+async function getData(id: string) {
+  try {
+    const response = await fetch(
+      `https://655c87bc25b76d9884fd79b6.mockapi.io/Team/${id}`
+    );
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function generateMetadata({
+  params: { id },
+}: Props): Promise<Metadata> {
+  const post: postType = await getData(id);
+  return {
+    title: post.name,
+  };
+}
+
+export default async function TeamComponentIn({ params: { id } }: Props) {
+  const post: postType = await getData(id);
+  return (
+    <div
+      className={styles.teamIn}
+      style={{
+        backgroundImage: "url(/images/our-bg.jpg)",
+        backgroundSize: "cover",
+      }}>
+      <div className={styles.teamIn__container}>
+        <h1>{post.name}</h1>
+        <img src={`${post.imageUrl}.jpg`} alt="imgName" />
+        <p>{post.text}</p>
+        <button className={styles.teamIn__button}>
+          <Link href="/team">Back</Link>
+        </button>
+      </div>
+    </div>
+  );
+}
